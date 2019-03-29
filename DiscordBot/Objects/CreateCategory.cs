fccf1a2b0c9631557b1803437c966ee658246e42
Marius Kristiansen <mariussk@getacademy.no>
2019-03-29 14:41:48 +0100

    c

--- DiscordBot/Objects/CreateCategory.cs
+++ DiscordBot/Objects/CreateCategory.cs
@@ -1,4 +1,7 @@
-﻿namespace DiscordBot.Objects
+﻿using System.Collections.Generic;
+using Discord;
+
+namespace DiscordBot.Objects
 {
     /*
      * This Object is used to create a collection of channels with proper permissions under a given Category name
@@ -6,10 +9,33 @@
     public class CreateCategory
     {
         private string _name;
+        private ICategoryChannel _category;
+        private List<IChannel> _channels;
 
         public CreateCategory(string inputName)
         {
             _name = inputName;
         }
+
+        public void AddChannel(ChannelType type, ICategoryChannel defaultChannel = null)
+        {
+            if (defaultChannel != null)
+            {
+                /*
+                 * If the channel object/id is given, add a new channel to this object.
+                 */
+            }
+        }
+
+        public void Initialize()
+        {
+            /*
+             * Pushes the category to the cannel
+             */
+            var server = Program.GetServer;
+            //server.CreateCategoryChannelAsync(_name, properties => )
+        }
+
+        
     }
 }
