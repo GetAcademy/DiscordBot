a0f3b3fc7f56f5aa1318873f8765b393bd20dc5e
Marius Kristiansen <mariussk@getacademy.no>
2019-03-29 12:38:22 +0100

    Create CreateCategory.cs

---
+++ DiscordBot/Objects/CreateCategory.cs
@@ -0,0 +1,15 @@
+﻿namespace DiscordBot.Objects
+{
+    /*
+     * This Object is used to create a collection of channels with proper permissions under a given Category name
+     */
+    public class CreateCategory
+    {
+        private string _name;
+
+        public CreateCategory(string inputName)
+        {
+            _name = inputName;
+        }
+    }
+}
